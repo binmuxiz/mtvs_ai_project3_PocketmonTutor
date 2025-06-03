8847904b74abc5123a15c0876a5d98ffac91e958
binmuxiz <binmuxiz@gmail.com>
2025-06-03 18:32:47 +0900

    todo 테이블 추가

--- backend/db.py
+++ backend/db.py
@@ -11,7 +11,7 @@ def init_db():
     conn = get_connection()
     cur = conn.cursor()
 
-    # users 테이블 생성
+# 유저 테이블 생성
     cur.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,6 +20,7 @@ def init_db():
         );
     """)
 
+# 유저 포켓몬 테이블
     cur.execute("""
         CREATE TABLE IF NOT EXISTS user_pokemons (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,6 +32,18 @@ def init_db():
             match_json TEXT,
             image TEXT,
             model_file_path TEXT,
+            FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+        );
+    """)
+
+# 투두 테이블
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS todos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id TEXT NOT NULL,
+            title TEXT NOT NULL,
+            complete INTEGER DEFAULT 0,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
         );
     """)
