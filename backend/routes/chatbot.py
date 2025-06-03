56c2f668dd7cdfb565ae01968a9e5a88cd9c7f9b
binmuxiz <binmuxiz@gmail.com>
2025-06-03 18:36:46 +0900

    챗봇 api 생성

---
+++ backend/routes/chatbot.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+
+from pydantic import BaseModel, Field
+
+from agent.chatbot_agent import invoke_agent
+
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+class ChatPrompt(BaseModel):
+    user_id: str = Field(..., description="사용자 고유 ID")
+    text: str = Field(..., description='chatbot에게 하고 싶은 말')
+
+
+@router.post("/")
+async def chatbot(request: ChatPrompt) -> str:
+    user_input = request.text
+    user_id = request.user_id
+
+    output = invoke_agent(user_input, session_id=user_id)
+    return output
