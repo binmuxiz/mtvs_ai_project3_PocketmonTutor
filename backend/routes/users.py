2bfe3b124ce1fcaaac70ce40bf8e56b79c0f5192
binmuxiz <binmuxiz@gmail.com>
2025-06-04 18:41:32 +0900

    user repository 분리

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,43 +1,26 @@
 from fastapi import APIRouter, HTTPException
 from models import UserCreate
-from db import get_connection
-
+from db import get_connection, get_user_by_id, create_user
 router = APIRouter()
-
 
 
 
 @router.post("/")
 async def generate(data: UserCreate):
     
-    conn = get_connection()
+# 유저 존재 확인
+    user_row = get_user_by_id(data.user_id)
 
+    if user_row:
+        raise HTTPException(status_code=400, detail="이미 존재하는 사용자 ID입니다.")
+
+# 없으면 등록
     try:
-        cur = conn.cursor()
-
-        # 사용자 존재 확인
-        cur.execute("SELECT * FROM users WHERE user_id = ?", (data.user_id,))
-
-        if cur.fetchone():
-            raise HTTPException(status_code=400, detail="이미 존재하는 사용자 ID입니다.")
-
-        cur.execute("""
-            INSERT INTO users (user_id, name)
-            VALUES (?, ?)
-        """, (
-            data.user_id,
-            data.name
-        ))
-        conn.commit()
-
+        create_user(data)
         print("사용자 등록 완료 id = ", data.user_id)
 
     except Exception as e:
-        conn.rollback()
-        raise 
-
-    finally:
-        conn.close()
+        raise
 
     return {
         "message": "사용자 등록 완료!",
